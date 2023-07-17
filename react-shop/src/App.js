c110ed342ed4c06bc16f39ca50889452911a60bc
Makoni1 <67749844+Makoni1@users.noreply.github.com>
2023-07-18 03:59:40 +0600

    подключила router 6

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,44 +1,23 @@
-import Categories from './components/Categories';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Sort from './components/Sort';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+// import NotFound from './pages/NotFound';
+
 import './scss/app.scss';
-// import pizzas from './assets/pizzas.json';
-import { useEffect, useState } from 'react';
-
-
+import { Route, Routes } from 'react-router-dom';
 function App() {
-  const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:2323/pizzas')
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-      });
-  }, []);
-  
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock
-                // title={obj.title}
-                // price={obj.price}
-                // imageUrl={obj.imageUrl}
-                // sizes={obj.sizes}
-                {...obj}
-              />
-            ))}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
